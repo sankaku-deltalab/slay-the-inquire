409872cb2566f665439db4d33e174df8ee1ca304
Sankaku <sankaku_dlt.45631@outlook.jp>
2023-09-14 08:04:52 +0000

    Add keyword search

--- src/search.ts
+++ src/search.ts
@@ -18,10 +18,35 @@ export type TextRange = {
 export namespace Search {
   export function searchTargets(
     uri: Uri,
-    workspaceState: TextDocument
+    workspaceState: TextDocument,
+    keywords: string[]
   ): TextRange[] {
+    return searchKeywords(uri, workspaceState.getText(), keywords);
     return searchFunctionNameRanges(uri, workspaceState.getText());
     // TODO: get tooltip text
+  }
+
+  function searchKeywords(
+    uri: Uri,
+    text: string,
+    keywords: string[]
+  ): TextRange[] {
+    const rangesMut: TextRange[] = [];
+    keywords.forEach((keyword) => {
+      const regex = new RegExp(keyword, "gi");
+
+      // regex: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec
+      let array1;
+      while ((array1 = regex.exec(text)) !== null) {
+        rangesMut.push({
+          uri: uri,
+          start: regex.lastIndex - keyword.length,
+          end: regex.lastIndex,
+          text: keyword,
+        });
+      }
+    });
+    return rangesMut;
   }
 
   function searchFunctionNameRanges(uri: Uri, text: string): TextRange[] {
