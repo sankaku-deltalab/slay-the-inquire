3e9d11c0e5b8a533086648bf34e208ecb62f9f6a
Sankaku <sankaku_dlt.45631@outlook.jp>
2023-09-14 08:14:49 +0000

    Add sample tooltip activated by command

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,48 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { Search } from "./search";
+
+const KEYWORDS_FOR_SAMPLE: Record<string, string> = {
+  hello: "Text for sample",
+  disposable: "Subscribable items?",
+};
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "slay-the-inquire.helloWorld",
+    helloWorld
+  );
+  context.subscriptions.push(disposable);
+}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "slay-the-inquire" is now active!');
+function helloWorld(context: vscode.ExtensionContext): void {
+  const keywords = KEYWORDS_FOR_SAMPLE;
+  const editor = vscode.window.activeTextEditor;
+  if (editor === undefined) {
+    return;
+  }
+  const document = editor.document;
+  const uri = document.uri;
+  const targets = Search.searchTargets(uri, document, Object.keys(keywords));
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('slay-the-inquire.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Slay the Inquire!');
-	});
-
-	context.subscriptions.push(disposable);
+  for (const t of targets) {
+    const startPos = editor.document.positionAt(t.start);
+    const endPos = editor.document.positionAt(t.end);
+    const range: vscode.DecorationOptions = {
+      range: new vscode.Range(startPos, endPos),
+      hoverMessage: new vscode.MarkdownString(
+        `*${t.text}*\n\n${keywords[t.text]}`
+      ),
+    };
+    const deco = vscode.window.createTextEditorDecorationType({
+      textDecoration: "underline",
+    });
+    editor.setDecorations(deco, [range]);
+  }
+  vscode.window.showInformationMessage("decorated");
 }
 
 // This method is called when your extension is deactivated
